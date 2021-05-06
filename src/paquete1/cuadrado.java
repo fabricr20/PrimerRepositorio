f916f827b5c181539e3baa1afbb2935d7a547cb4
Fabricio Conde Rojas <fabricr20@gmail.com>
2021-05-06 09:19:44 +0200

    Creada clase cuadrados

---
+++ src/paquete1/cuadrado.java
@@ -0,0 +1,14 @@
+package paquete1;
+
+public class cuadrado {
+	double lado;
+	
+	public cuadrado() {}
+	public cuadrado(double milado) {
+		lado=milado;
+	}
+	public double calcularPerimetro() {
+		return 4*lado;
+	}
+
+}
