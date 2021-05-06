4fefcb0607ebe27968ca8ac10521a9bedcf464ac
Fabricio Conde Rojas <fabricr20@gmail.com>
2021-05-06 09:11:02 +0200

    first commit

---
+++ src/paquete1/principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package paquete1;
+
+/**
+ * @author Usuario
+ *
+ */
+public class principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hola Mundo");
+
+	}
+
+}
